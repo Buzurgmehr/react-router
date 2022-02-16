833bbda49d96f8d520f0d06c3bbda42aefd3e09e
Buzurgmehr <kholiqovbuzurgmehr59@gmail.com>
2022-02-16 14:20:53 +0500

    new commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2 style={{ marginTop: "1rem", textAlign: "center" }}>About</h2>
+            <p style={{ marginTop: "1rem",textAlign: "center"}}>lorem ipsum dolor sit amet, consectetur</p>
+        </main>
+    )
+}
+
+export default About
